ba01e76c2240219da1391a9f22ec84c6b6e8e190
KyleValkyrie <baolegend9@gmai.com>
2019-12-05 17:11:08 +0700

    Second steps

---
+++ New Unity Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    float speed = 0.25f;
+    float velocity = 0.25f;
+    float smooth = 5.0f;
+    float tiltAngle = 60.0f;
+
+    void Update()
+    {
+        //Plane movements
+        float moveHorizontally = Input.GetAxis("Horizontal");
+        float moveVertically = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontally*velocity, moveVertically* -1*velocity, speed);
+        transform.position += movement;
+        
+
+        // Smoothly tilts the plane towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        // Rotate the plane by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, -tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+       
+
+        
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerPOV.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPOV : MonoBehaviour
+{
+    public Transform targetPlayer;
+    public Vector3 distance = new Vector3(0f, 5f, -10f);
+    public float positionDampening = 1.5f;
+    public float rotationDampening = 1.5f;
+    private Transform thisTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetPlayer==null)
+        {
+            return;
+        }
+        Vector3 wantedPosition = targetPlayer.position + (targetPlayer.rotation * distance);
+        Vector3 currentPosition = Vector3.Lerp(thisTransform.position, wantedPosition, positionDampening * Time.deltaTime);
+        thisTransform.position = currentPosition;
+        Quaternion wantedRotation = Quaternion.LookRotation(targetPlayer.position = thisTransform.position, targetPlayer.up);
+        thisTransform.rotation = wantedRotation;
+    }
+}
