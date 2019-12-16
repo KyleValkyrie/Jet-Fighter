6ebfd434c7b7b3f74c1b7b74934c9d379339f3e0
KyleValkyrie <baolegend9@gmai.com>
2019-12-16 17:08:21 +0700

    redo the camera script for better player tracking, also importing some nice skybox texture and player models

--- New Unity Project/Assets/Scripts/PlayerPOV.cs
+++ New Unity Project/Assets/Scripts/PlayerPOV.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class PlayerPOV : MonoBehaviour
 {
     public Transform targetPlayer;
-    public Vector3 distance = new Vector3(0f, 5f, -10f);
-    public float positionDampening = 1.5f;
-    public float rotationDampening = 1.5f;
+    public Vector3 distance= new Vector3(0f, 1.5f, -10f);
+    public float positionDamping = 2f;  
+    public float rotationDamping = 2f;
     private Transform thisTransform;
-    // Start is called before the first frame update
     void Start()
     {
         thisTransform = transform;
     }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (targetPlayer==null)
+        if (targetPlayer == null)
         {
             return;
         }
         Vector3 wantedPosition = targetPlayer.position + (targetPlayer.rotation * distance);
-        Vector3 currentPosition = Vector3.Lerp(thisTransform.position, wantedPosition, positionDampening * Time.deltaTime);
+
+        Vector3 currentPosition = Vector3.Lerp(thisTransform.position, wantedPosition, positionDamping * Time.deltaTime);
+
         thisTransform.position = currentPosition;
-        Quaternion wantedRotation = Quaternion.LookRotation(targetPlayer.position = thisTransform.position, targetPlayer.up);
+
+        Quaternion wantedRotation = Quaternion.LookRotation(targetPlayer.position - thisTransform.position);
         thisTransform.rotation = wantedRotation;
-    }
+     }
 }
